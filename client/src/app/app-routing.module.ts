5203f0f9c931ea7e3650c3166807a9c7b177e06b
TuanLA53 <TuanLA53@gmail.com>
2024-05-13 00:41:09 +0700

    add error validation client

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { TestErrorComponent } from './errors/test-error/test-error.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -20,7 +21,9 @@ const routes: Routes = [
       { path: 'messages', component: MessagesComponent },
     ],
   },
-
+  { path: 'errors', component: TestErrorComponent },
+  { path: 'not-found', component: TestErrorComponent },
+  { path: 'errors', component: TestErrorComponent },
   { path: '**', component: MessagesComponent, pathMatch: 'full' },
 ];
 
