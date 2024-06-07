d2edde42ba042c6f400525372382fc350b17efde
TuanLA <tuanla@v-acs.net>
2024-06-07 16:44:02 +0700

    add jwt interceptor

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -12,26 +12,13 @@ export class MembersService {
 
   getMembers() {
     return this.http.get<Member[]>(
-      this.baseUrl + 'users',
-      this.getHttpOptions()
+      this.baseUrl + 'users'
     );
   }
 
   getMember(username: string) {
     return this.http.get<Member>(
-      this.baseUrl + 'users/' + username,
-      this.getHttpOptions()
+      this.baseUrl + 'users/' + username
     );
-  }
-
-  getHttpOptions() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + user.token,
-      }),
-    };
   }
 }
