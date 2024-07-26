1ef538141591f678315d5d2551f304616043fb1b
TuanLA <tuanla@v-acs.net>
2024-07-26 15:27:27 +0700

    input common

--- client/src/app/_forms/text-input/text-input.component.ts
+++ client/src/app/_forms/text-input/text-input.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, Self } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-text-input',
   templateUrl: './text-input.component.html',
-  styleUrls: ['./text-input.component.css']
+  styleUrls: ['./text-input.component.css'],
 })
-export class TextInputComponent {
+export class TextInputComponent implements ControlValueAccessor {
+  @Input() label = '';
+  @Input() type = 'text';
+  constructor(@Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
+  }
+  writeValue(obj: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnChange(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
 
+  get control(): FormControl{
+    return this.ngControl.control as FormControl
+  }
 }
